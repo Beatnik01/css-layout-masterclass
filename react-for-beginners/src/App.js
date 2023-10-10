6c3fd8faa442b99444e01b883bd27f7877001e21
Gihoon Lim <knv429@naver.com>
2023-10-10 10:02:13 +0900

    macOs

--- react-for-beginners/src/App.js
+++ react-for-beginners/src/App.js
@@ -1,23 +1,32 @@
 import { useState, useEffect } from "react";
 
 function App() {
-  const [counter, setValue] = useState(0);
-  const [keyword, setKeyword] = useState("");
-  const onClick = () => setValue((prev) => prev + 1);
-  const onChange = (e) => setKeyword(e.target.value);
-  console.log("i run all the time");
-  const iRunOnlyOnece = () => {
-    console.log("only once");
+  const [toDo, setToDo] = useState("");
+  const [toDos, setToDos] = useState([]);
+  const onChange = (e) => setToDo(e.target.value);
+  const onSubmit = (e) => {
+    e.preventDefault();
+    if (toDo === "") {
+      return;
+    }
+    setToDos((currentArray) => [toDo, ...currentArray]);
+    setToDo("");
   };
-  useEffect(() => {
-    console.log("SEARCH FOR", keyword);
-  }, [keyword]);
-  useEffect(iRunOnlyOnece, []);
+  console.log(toDos);
+
   return (
     <div>
-      <input value={keyword} onChange={onChange} type="text" placeholder="Search here..." />
-      <h1>{counter}</h1>
-      <button onClick={onClick}>Click me!</button>
+      <h1>My To Dos ({toDos.length})</h1>
+      <form onSubmit={onSubmit}>
+        <input onChange={onChange} value={toDo} type="text" placeholder="Write ur to do..." />
+        <button>Add To Do</button>
+      </form>
+      <hr />
+      <ul>
+        {toDos.map((item, index) => (
+          <li key={index}>{item}</li>
+        ))}
+      </ul>
     </div>
   );
 }
