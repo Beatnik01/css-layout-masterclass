2a0089103330a62bad9ae004914a7f094aaac15b
Gihoon Lim <knv429@naver.com>
2023-10-13 23:46:25 +0900

    add detail

--- react-for-beginners/src/App.js
+++ react-for-beginners/src/App.js
@@ -1,44 +1,23 @@
-import { useState, useEffect } from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./routes/Home";
+import Detail from "./routes/Detail";
+import Error from "./routes/Error";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home></Home>,
+    errorElement: <Error></Error>,
+  },
+  {
+    path: "/movie/:id",
+    element: <Detail></Detail>,
+    errorElement: <Error></Error>,
+  },
+]);
 
 function App() {
-  const [loading, setLoading] = useState(true);
-  const [movies, setMovies] = useState([]);
-  const getMovies = async () => {
-    const response = await fetch(
-      `https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year`
-    );
-    const json = await response.json();
-    setMovies(json.data.movies);
-    setLoading(false);
-  };
-  useEffect(() => {
-    getMovies();
-  }, []);
-  console.log(movies);
-  return (
-    <div>
-      {loading ? (
-        <h1>Loading...</h1>
-      ) : (
-        <div>
-          {movies.map((movie) => (
-            <div key={movie.id}>
-              <img src={movie.medium_cover_image} />
-              <h2>{movie.title}</h2>
-              <p>{movie.summary}</p>
-              <ul>
-                <li>
-                  {movie.genres.map((g) => (
-                    <li>{g}</li>
-                  ))}
-                </li>
-              </ul>
-            </div>
-          ))}
-        </div>
-      )}
-    </div>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
