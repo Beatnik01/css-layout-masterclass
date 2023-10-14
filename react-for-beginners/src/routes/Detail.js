b6ba211d71e5750be25d05049e17cfeeb71cdb6d
Gihoon Lim <knv429@naver.com>
2023-10-14 10:20:34 +0900

    update Deatil

--- react-for-beginners/src/routes/Detail.js
+++ react-for-beginners/src/routes/Detail.js
@@ -1,18 +1,42 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import Movie from "../components/Movie";
 
 function Detail() {
   const { id } = useParams();
+  const [loading, setLoading] = useState(true);
+  const [movie, setMovie] = useState();
+
   const getMovie = async () => {
     const json = await (
       await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
     ).json();
-    console.log(json);
+    setMovie(json.data.movie);
+    setLoading(false);
   };
+
   useEffect(() => {
     getMovie();
   }, []);
-  return <h1>Detail</h1>;
+
+  return (
+    <div>
+      {loading ? (
+        <h1>Loading...</h1>
+      ) : (
+        <div>
+          <Movie
+            key={movie.id}
+            id={movie.id}
+            coverImg={movie.medium_cover_image}
+            title={movie.title}
+            summary={movie.description_intro}
+            genres={movie.genres}
+          />
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default Detail;
